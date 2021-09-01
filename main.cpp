6bd563c6336117099b3ab0a90be916d5c15f021f
Nastya10 <48963234+Nastya10@users.noreply.github.com>
2021-09-01 19:00:31 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,14 +17,18 @@ int main()
     char *buffer = copy_file(input_file1, &file_len);
     struct array_element *array_lines = read_lines(buffer, file_len, &len_array);
 
+    fprintf(input_file2, "The original:\n\n");
     fprint_array(array_lines, len_array, input_file2);
     fprintf(input_file2, "\n");
 
+    fprintf(input_file2, "Sorted alphabetically by the beginning of the line:\n\n");
     quick_sort(array_lines, 0, len_array - 1, BEG_OF_LINE);
     fprint_array(array_lines, len_array, input_file2);
     fprintf(input_file2, "\n");
 
+    fprintf(input_file2, "Sorted alphabetically by the end of the line:\n\n");
     quick_sort(array_lines, 0, len_array - 1, END_OF_LINE);
+
     fprint_array(array_lines, len_array, input_file2);
     fprintf(input_file2, "\n");
 
